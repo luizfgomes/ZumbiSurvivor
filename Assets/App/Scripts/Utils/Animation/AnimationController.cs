56790f3e66bd1eeda53051d91b5a91b0f5456a7e
Luiz Gomes <luiz.fernando2108@gmail.com>
2025-07-13 04:20:15 -0300

    Fix AnimationController

--- Assets/App/Scripts/Utils/Animation/AnimationController.cs
+++ Assets/App/Scripts/Utils/Animation/AnimationController.cs
@@ -6,7 +6,7 @@ public abstract class AnimationController : MonoBehaviour, IAnimationController
 
     protected virtual void Awake ()
     {
-        if ( !animator )
+        if ( animator )
             return;
         animator = GetComponentInChildren<Animator>();
     }
